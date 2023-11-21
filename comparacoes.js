0999f91eccc82889a065bfef09c55fc1dda361ba
Murilo Meneguetti Pegoraro <murilomeneguetti@gmail.com>
2023-11-21 16:04:28 -0300

    Create comparacoes.js

---
+++ comparacoes.js
@@ -0,0 +1,12 @@
+const numero = 5;
+const texto = '5';
+
+// == (comparação implícita, só compara o valor)
+console.log(numero == texto);
+
+// === comparação explícita, compara o valor e o tipo de dado
+console.log(numero === texto);
+
+//typeof
+console.log(typeof numero)
+console.log(typeof texto)
