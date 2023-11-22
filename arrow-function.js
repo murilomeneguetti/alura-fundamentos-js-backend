1bdb163f5718b4b0d93937c69665dc1c364456c6
Murilo Meneguetti Pegoraro <murilomeneguetti@gmail.com>
2023-11-21 21:12:24 -0300

    commit final

--- arrow-function.js
+++ arrow-function.js
@@ -6,7 +6,6 @@ const apresentarArrow = nome => `meu nome é ${nome}`;
 const soma = (num1, num2) => num1 + num2;
 
 //arrow function com + de uma linha de instrução
-
 const somaNumerosPequenos = (num1, num2) => {
     if (num1 > 10 || num2 > 10) {
         return 'somente numeros de 1 a 9';
